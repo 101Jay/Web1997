203aed3b6ecd14ce3425d44c9aa8bab166d1fa36
kjk**** <kjk8569@naver.com>
2020-12-15 14:45:41 +0000

    if you want to be a programmer, do not make your code repeat itself

---
+++ Web/colors.js
@@ -0,0 +1,45 @@
+var body = {};
+		
+		body.setColor = function(color){
+			document.querySelector('body').style.color=color;
+		}
+		body.setBackColor = function(color){
+			document.querySelector('body').style.backgroundColor=color;
+		}
+		
+	function daynight(self)
+		{
+		var target = document.querySelector('body'); 
+			
+		if(self.value ==='밤'){
+		body.setBackColor('black')
+		body.setColor('green')
+		
+		self.value ='낮'	
+		 
+		var links = document.querySelectorAll('a');
+		var i = 0;
+		while(i<links.length){
+			if (links[i].id==='enter'){
+				links[i].style.color='green';
+			}
+			else{
+				links[i].style.color='white';
+			}
+		 i=i+1;
+		 }
+		 }	
+		else{
+		body.setBackColor('white')
+		body.setColor('black')
+		self.value ='밤';		
+		
+		var links = document.querySelectorAll('a');
+		var i = 0;
+		while(i<links.length){
+		 links[i].style.color='blue';
+		 i=i+1;
+		 }
+		 
+		 }	
+		}

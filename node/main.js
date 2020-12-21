45adb5de9371ebdc32a23d1d765fc85624e89502
kjk**** <kjk8569@naver.com>
2020-12-21 14:59:24 +0000

    node.js

--- node/main.js
+++ node/main.js
@@ -1,18 +1,72 @@
 var http = require('http');
 var fs = require('fs');
+var url= require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
-    }
-    if(request.url == '/favicon.ico'){
-      	response.writeHead(404);
-		response.end();
-		return;
-    }
-    response.writeHead(200);
-	console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
+var _url = request.url;
+var queryData = url.parse(_url,true).query;
+
+var pathname = url.parse(_url,true).pathname;
+
+if(pathname==='/'){
+	if(queryData.id===undefined){
+		var title = 'Welcome';
+		var description ='Nice to meet you';
+		
+		var template = `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>WEB1 - ${title}</title> 
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  <ol>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavaScript">JavaScript</a></li>
+  </ol>
+  <h2>${title}</h2>  
+  <p>${description}</p>
+</body>
+</html>
+`;
+	
+	response.writeHead(200);
+    response.end(template);
+	}else{
+	fs.readFile(`Data/${queryData.id}`,'utf8', function(err,description){  // 색은 저렇게 떠도 작동은 한다.
+	var title = queryData.id;	
+	var template = `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>WEB1 - ${title}</title> 
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  <ol>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavaScript">JavaScript</a></li>
+  </ol>
+  <h2>${title}</h2>  
+  <p>${description}</p>
+</body>
+</html>
+`;
+	
+	response.writeHead(200);
+    response.end(template);
+	})
+	}	
+}else{
+	response.writeHead(404);
+    response.end("File NOT FOUND");
+}
+
  
 });
 app.listen(3000);
--- syntax/str.js
+++ syntax/str.js
@@ -1,7 +1,12 @@
 var name = "node.js";
 
-var text = 	`Dear ${name} 
+var text = 	`Dear ${name} 	
 hihihihihihisosososoolonh
-hihihihihislslso${name}`;
+hihihihihislslso${name}`; //`의 효과!
 
-console.log(text);
+console.log(text+"hahaha");
+
+//template literal까지
+
+var args = process.argv;
+console.log(args[2]);
